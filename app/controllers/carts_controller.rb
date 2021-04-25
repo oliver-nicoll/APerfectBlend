d14bf16369a91bb917e116eea579b3982f53e216
Nicoll Oliver <nicolldon96@gmail.com>
2021-04-25 17:30:48 -0400

    carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,33 @@
 class CartsController < ApplicationController
+    
+    def new
+        @cart = Cart.new
+    end
+
+    def create
+        @cart = Cart.new(cart_params)
+
+        if @cart.save 
+            redirect_to cart_path(@cart)
+        else
+            render :new
+        end
+    end
+
+    def index
+        @cart = Cart.all
+    end
+
+    def show
+        @cart = Cart.find(params[:id])
+    end
+
+    def checkout
+
+    end
+
+    private 
+    def cart_params
+        params.require(:cart).permit(:user_id, :checkout)
+    end
 end
